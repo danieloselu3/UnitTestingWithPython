5c22bd6820b3d06c1ccb78fd0b5df36657ce5f0a
danieloselu3 <doselu3@gmail.com>
2018-12-29 06:11:27 +0300

    Initial commit

---
+++ test_foo.py
@@ -0,0 +1,19 @@
+#test_foo.py
+import unittest
+
+#function to be tested
+def add(a, b):
+    return a + b
+
+class FooTestCase(unittest.TestCase):
+
+    #test method
+    def test_add(self):
+        sum = add(1, 2)
+
+        #validate the result of the code we're testing.
+        self.assertEqual(3, sum)
+
+    def test_add_negative_numbers(self):
+        sum = add(-1, -2)
+        self.assertEqual(-3, sum)
